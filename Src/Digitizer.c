7a503e741a7ab87fa3e9dcb3b44b572cd6cadc40
Henry Spencer <henry.spencer@touchnetix.com>
2026-08-04 15:54:52 +0100

    Restored original digitizer touch processing.

--- Src/Digitizer.c
+++ Src/Digitizer.c
@@ -365,25 +365,13 @@ void MultiPointDigitizer(void)
             {
                 if(GetXYZFromReport(DO_NOT_IGNORE_COORDS, byTouchNum))
                 {
-                    // Valid touch detected, so process it
-                    
-                    // Extract region info from X/Y
-                    volatile uint8_t region = ((DigitizerYCoord & 0x3) << 2) | (DigitizerXCoord & 0x3);
-                    if(region != 0)
+                    if(byReportZ_lsb >= 0x80)   // if z coordinate is a negative value it indicates there is a hover or prox
                     {
-                        // Skip this touch if not region zero
-                        touched = CONFIDENCE; // Set confidence bit only
+                        touched = CONFIDENCE | IN_RANGE; // hover present --> set in range bit
                     }
                     else
                     {
-                        if(byReportZ_lsb >= 0x80)   // if z coordinate is a negative value it indicates there is a hover or prox
-                        {
-                            touched = CONFIDENCE | IN_RANGE; // hover present --> set in range bit
-                        }
-                        else
-                        {
-                            touched = CONFIDENCE | IN_RANGE | TIP_SWITCH; // touch present --> set tip switch bit
-                        }
+                        touched = CONFIDENCE | IN_RANGE | TIP_SWITCH; // touch present --> set tip switch bit
                     }
                 }
                 else
